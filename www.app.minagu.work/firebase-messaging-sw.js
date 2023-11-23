248df924ef852f2948b251ab276d7ce5b858e321
minamiguchi <kazu9sum.mi@gmail.com>
2023-11-23 19:29:13 +0900

    add FCM file

---
+++ www.app.minagu.work/firebase-messaging-sw.js
@@ -0,0 +1,16 @@
+// Push通知を受け取ると呼ばれる
+self.addEventListener('push', function (event) {
+  // メッセージを表示する
+  console.log(event)
+  var data = {};
+  if (event.data) {
+    data = event.data.json();
+  }
+  var title = data.notification.title || "無題";
+  var message = data.notification.body || "メッセージが届いています。";
+  event.waitUntil(
+    self.registration.showNotification(title, {
+      'body': message
+    })
+  );
+});
